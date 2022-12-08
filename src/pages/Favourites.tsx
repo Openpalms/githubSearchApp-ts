8f6d4a4bf90f0f85188d26bc57328a8e9079d76e
Galochkin Daniil <galo4kin123danilo@gmail.com.com>
2022-12-08 23:51:07 +0300

    added deboucing on search and dropdown

---
+++ src/pages/Favourites.tsx
@@ -0,0 +1,13 @@
+const Favourites = () => {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem labore,
+        quisquam ipsa temporibus expedita explicabo. Quae, culpa. Officia beatae
+        minus vel at excepturi rem reprehenderit maiores nesciunt veniam! Dolor
+        cumque consequatur quia iusto illum dolores rem mollitia ea quos labore.
+      </p>
+    </>
+  );
+};
+export default Favourites;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import { useDebounce } from '../hooks/debounce';
+import { useSearchUsersQuery } from '../store/github/githubAPI';
+
+const HomePage = () => {
+  const [search, setSearch] = useState('');
+  const [dropdown, setDropdown] = useState(false);
+  const debounced = useDebounce(search);
+  const { isLoading, isError, data } = useSearchUsersQuery(debounced, {
+    skip: debounced.length < 2,
+  });
+  useEffect(() => {
+    setDropdown(debounced.length > 2 && data?.length! > 0);
+  }, [debounced, data]);
+  return (
+    <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
+      {isError && <p className="text-center text-red-600">Error</p>}
+      <div className="relative w-[560px] ">
+        <input
+          type="text"
+          className="border px-4 py-2 w-[100%] h-[42px] mb-2"
+          placeholder="search for github users"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        {dropdown && (
+          <ul className="list-none absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white p-5 overflow-y-scroll">
+            {isLoading && <p className="text-center"> Loading...</p>}
+            {data?.map((user) => (
+              <li
+                className="py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer "
+                key={user.id}
+              >
+                {user.login}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+export default HomePage;
